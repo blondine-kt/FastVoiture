28265b4ad191bcbd96f88cb4058d15bc920f01dd
Dieudonne26 <precieukosso@teccart.online>
2024-10-17 08:31:59 -0400

    add the server for connect front and back

--- FastVoiture/FaceRecognition/server.py
+++ FastVoiture/FaceRecognition/server.py
@@ -1,9 +1,9 @@
+""""
 from flask import Flask, request, jsonify
 from function import saved,login
 
 app = Flask(__name__)
 
-data_stock={}
 
 @app.route('/register', methods=['POST'])
 def saved_face(names,url):
@@ -18,3 +18,84 @@ def login_face(url):
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
+
+from fastapi import FastAPI,Depends
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sqlmodel import SQLModel,Field,create_engine,Session
+from typing import Annotated
+from.securite import password_hash,password_verify
+from.models import Driver,Con,Drivers
+app=FastAPI()
+app.add_middleware ( CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]) #permet la communication avec react 
+
+# connection a la Base de donnees
+url="mssql+pyodbc://(LocalDB)\\MSSQLLocalDB/FastVoiture?driver=ODBC+Driver+17+for+SQL+Server" 
+engine=create_engine(url)
+
+#creation de tables
+def create_table():
+     SQLModel.metadata.create_all(engine)
+    
+def get_session():
+     with Session(engine) as session:
+          yield session
+SessionDep=Annotated[Session,Depends(get_session)]
+
+
+@app.on_event("startup")
+async def on_startup():
+     create_table()
+
+
+def user_verify(item,session:SessionDep):
+     users=session.query(Drivers).filter(Drivers.userName==item).first()
+     return users
+     
+
+@app.post("/")
+async def Register (user:Driver,session:SessionDep):
+        new_user=Drivers(
+             userName=user.userName,
+             password=password_hash( user.password),
+             nom=user.nom,
+             email=user.email,
+             phone=user.phone,
+             license_plate=user.license_plate,
+             driver_license=user.driver_license)
+        
+        if user_verify(new_user.userName,session):
+             response={"message":"nom d'utiliateur deja existant "}
+        else:
+             
+            session.add(new_user)
+            session.commit()
+            session.refresh(new_user)
+            response={"message":"bien ajoute","user":new_user}
+        return response
+        
+@app.post("/Login")
+async def connexion(user:Con, session:SessionDep):
+       new_user=Drivers(userName=user.userName,password=user.password)
+       
+       users=session.query(Drivers).filter(Drivers.userName==new_user.userName).first()
+       
+       if users:
+            verify=password_verify(new_user.password,users.password)
+            
+            if verify:
+                return{"message":"bonne connexion"}
+            else:
+                 return{"message":"mot de passe incorrect"}
+
+       else:
+            return{"message":"personne inexistante"}
+           
+if __name__=="__name__":
+    uvicorn.run(app,host="0.0.0.0", port=8022, workers=1)
